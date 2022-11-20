5c974b392b0f9f52084ee5a4c496a95a07a50108
mmemoribell <mmemoriibell@gmail.com>
2022-11-20 14:31:56 -0500

    yup

---
+++ IfScripts/min_max.js
@@ -0,0 +1,16 @@
+let a = 53;
+let b = 34;
+let c = 98;
+
+if ( c <= 50) {
+    console.log("works?");
+
+} else if ( a || c >= 80 ){
+    console.log("no");
+
+} else if ( a || c <= 100) {
+     console.log("works");
+
+} else {
+    console.log("result");
+}
