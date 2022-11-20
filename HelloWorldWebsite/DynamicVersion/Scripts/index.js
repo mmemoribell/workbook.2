39e3f2412c651f895e39190542923523c05ff052
mmemoribell <mmemoriibell@gmail.com>
2022-11-20 18:50:47 -0500

    yes

---
+++ HelloWorldWebsite/DynamicVersion/Scripts/index.js
@@ -0,0 +1,12 @@
+"Use Strict";
+
+window.onload = init;
+
+   function init() {
+    const helloBtn = document.getElementById("helloBtn");
+    helloBtn.onclick = onHelloBtnClicked;
+   }
+
+function onHelloBtnClicked() {
+    alert("Hi there!");
+}
