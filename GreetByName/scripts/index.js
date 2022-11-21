3c71b38393b0ac8b4ba7a345af2e61b0c6e4d1d2
mmemoribell <mmemoriibell@gmail.com>
2022-11-20 19:09:23 -0500

    yes

--- GreetByName/scripts/index.js
+++ GreetByName/scripts/index.js
@@ -1,2 +1,11 @@
 window.onload = init;
-init.onGreetUserBtnClicked()
+// init.onGreetUserBtnClicked()
+const name = document.getElementById("nameField");
+  function init() {
+    const greetBtn = document.getElementById("greetBtn");
+    greetBtn.onclick = onGreetBtnClicked;
+  }
+  function onGreetBtnClicked() {
+    const name = document.getElementById("nameField").value;
+    alert(name + " , un gusto conocerte");
+  }
